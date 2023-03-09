4820bd192b4032988db71f99d00b77d6ba9d006a
changye <1421698291@qq.com>
2023-03-09 23:05:30 +0800

    uuu

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # shuting-yard
 Implement shutting_yard algorithm using python
+
+token -> AST
+
+[] -> 递归 -> tree
+
+文法： 语言的规则 
+
+BNF 巴克斯范式 可以通过工具生成解析代码
+
+上下文无关文法 这里不是标准的BNF写法
--- parse/basic.py
+++ parse/basic.py
@@ -16,6 +16,11 @@ TT_MUL = "MUL"
 TT_DIV = "DIV"
 TT_LPAREN = "LPAREN"
 TT_RPAREN = "RPAREN"
+TT_EOF = "EOF"
+
+"""
+自定义Error
+"""
 
 
 class Error(object):
@@ -43,11 +48,33 @@ class IllegalCharError(Error):
         super().__init__(pos_start, pos_end, "Illegal Character", details)
 
 
+class InvalidSyntaxError(Error):
+    # 无效语法
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, "Invalid Syntax", details)
+
+
 class Token(object):
     # <token-name attr-value>
-    def __init__(self, type_, value=None):
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+        """
+        :param type_:
+        :param value:
+        :param pos_start: Position object
+        :param pos_end:
+        """
         self.type = type_
         self.value = value
+
+        if pos_start:
+            # Token 单个字符 + => pos_start = pos_end , advance
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance(self.value)  # 下一个token
+
+        # Token 多个字符 123.122
+        if pos_end:
+            self.pos_end = pos_end
 
     def __repr__(self):
         # 方便调试 看信息
@@ -84,7 +111,13 @@ class Position(object):
         # return Position(self.idx, self.ln, self.col, self.fn, self.txt)
 
 
+"""
+   词法分析器
+"""
+
+
 class Lexer(object):
+
     def __init__(self, fn, text):
         self.fn = fn
         self.text = text
@@ -92,6 +125,7 @@ class Lexer(object):
         self.current_char = None
         self.advance()
 
+    # 预读 独取下一个
     def advance(self):
         self.pos.advance(self.current_char)
         if self.pos.idx < len(self.text):
@@ -112,22 +146,22 @@ class Lexer(object):
             elif self.current_char in DIGITS:  # 数字
                 tokens.append(self.make_number())
             elif self.current_char == '+':
-                tokens.append(Token(TT_PLUS))
+                tokens.append(Token(TT_PLUS, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '-':
-                tokens.append(Token(TT_MINUS))
+                tokens.append(Token(TT_MINUS, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '*':
-                tokens.append(Token(TT_MUL))
+                tokens.append(Token(TT_MUL, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '/':
-                tokens.append(Token(TT_DIV))
+                tokens.append(Token(TT_DIV, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '(':
-                tokens.append(Token(TT_LPAREN))
+                tokens.append(Token(TT_LPAREN, pos_start=self.pos))
                 self.advance()
             elif self.current_char == ')':
-                tokens.append(Token(TT_RPAREN))
+                tokens.append(Token(TT_RPAREN, pos_start=self.pos))
                 self.advance()
             else:
                 # 没有匹配到 非法字符错误
@@ -135,6 +169,7 @@ class Lexer(object):
                 char = self.current_char
                 self.advance()
                 return [], IllegalCharError(pos_start, self.pos, f"'{char}")
+        tokens.append(Token(TT_EOF, pos_start=self.pos))
         return tokens, None
 
     def make_number(self):
@@ -156,7 +191,177 @@ class Lexer(object):
             return Token(TT_FLOAT, float(num_str))
 
 
+"""
+AST 节点
+"""
+
+
+class NumberNode(object):
+    def __init__(self, token):
+        self.token = token
+
+    def __repr__(self):
+        return f'{self.token}'
+
+
+class BinOpNode(object):
+    # 二元操作 + - * /， 1+2 left_node = 1, op_token = +, right_node = 2
+    def __init__(self, left_node, op_token, right_node):
+        self.right_node = right_node
+        self.op_token = op_token
+        self.left_node = left_node
+
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_token}, {self.right_node})'
+
+
+class UnaryOpNode(object):
+    #  一元操作 -1  op_token = -, node = 1
+    def __init__(self, op_token, node):
+        self.op_token = op_token
+        self.node = node
+
+    def __repr__(self):
+        return f'{self.op_token}, {self.node}'
+
+
+"""
+语法解析结果类
+"""
+
+
+class ParserResult(object):
+    def __init__(self):
+        self.error = None
+        self.node = None
+
+    def success(self, node):
+        self.node = node
+        return self
+
+    def failure(self, error):
+        self.error = error
+        return self
+
+    def register(self, res):
+        if isinstance(res, ParserResult):
+            if res.error:
+                self.error = res.error
+            return res.node
+        return res
+
+
+"""
+语法解析器
+"""
+
+
+class Parser(object):
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_idx = -1
+        self.advance()
+
+    def advance(self):
+        self.token_idx += 1
+        if self.token_idx < len(self.tokens):
+            self.current_token = self.tokens[self.token_idx]
+        return self.current_token
+
+    def parse(self):
+        res = self.expr()
+        if not res.error and self.current_token.type != TT_EOF:
+            # 返回报错
+            return res.failure(InvalidSyntaxError(
+                self.current_token.pos_start, self.current_token.pos_end,
+                "Expected '+','-','*' or '/'"
+            ))
+        return res
+
+    def factor(self):
+        """
+        factor -> INT | FLOAT
+               -> (PLUS | MINUS) factor
+               -> LPAREN expr RPAREN
+        :return:
+        """
+        res = ParserResult()
+        token = self.current_token
+
+        """
+        1 + 1
+        INT -> 1
+        token = +
+        并不需要error判断 因为只是一个简单的token
+        
+        -1 + 1
+        MINUS -> - 
+        token = 1
+        token = factor
+        factor 非终止符
+        因为它是非终止符 会继续进行匹配 匹配的过程中 可以发现error
+        所以要进行error的判断
+        """
+        if token.type in (TT_INT, TT_FLOAT):
+            # factor -> INT | FLOAT
+            res.register(self.advance())
+            return res.success(NumberNode(token))
+
+        elif token.type in (TT_PLUS, TT_MINUS):
+            # (PLUS | MINUS) factor
+            res.register(self.advance())
+            factor = res.register(self.factor())
+            if res.error:
+                return res
+            return res.success(UnaryOpNode(token, factor))
+
+        elif token.type == TT_LPAREN:
+            res.register(self.advance())
+            expr = res.register(self.expr())
+            if res.error:
+                return res
+
+            if self.current_token.type == TT_RPAREN:
+                res.register(self.advance())
+                return res.success(expr)
+            else:
+                # 返回报错
+                return res.failure(InvalidSyntaxError(
+                    self.current_token.pos_start, self.current_token.pos_end,
+                    "Expected ')'"
+                ))
+        return res.failure(InvalidSyntaxError(
+            self.current_token.pos_start, self.current_token.pos_end,
+            "Expected int or float"
+        ))
+
+    def term(self):
+        # term -> factor ((MUL | DIV) facter)*
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+
+    def expr(self):
+        # term((PLUS | MINUS) term)*
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+
+    def bin_op(self, func, ops):
+        # 递归调用 构建AST
+        res = ParserResult()
+        # 1+1
+        # 1
+        # advance -> +
+        left = res.register(func())
+        while self.current_token.type in ops:
+            op_token = self.current_token
+            res.register(self.advance())
+            right = res.register(func())
+            left = BinOpNode(left, op_token, right)
+        return res.success(left)
+
+
 def run(fn, text):
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
-    return tokens, error
+    # 生成AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    return ast.node, ast.error
